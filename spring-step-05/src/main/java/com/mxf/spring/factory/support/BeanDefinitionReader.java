d645394f3e99e81215938d43f5c032f492317e1e
manxingfu <manxingfu@sinandata.com>
2023-04-27 18:14:15 +0800

    自定义资源加载器解析文件，注册对象

--- spring-step-05/src/main/java/com/mxf/spring/factory/support/BeanDefinitionReader.java
+++ spring-step-05/src/main/java/com/mxf/spring/factory/support/BeanDefinitionReader.java
@@ -7,7 +7,7 @@ import com.mxf.spring.core.io.ResourceLoader;
 /**
  * @author manxingfu
  * @date 2023/4/27 16:03
- * @desc Bean定义信息读取接口
+ * @desc Bean定义信息读取接口-
  */
 public interface BeanDefinitionReader {
     BeanDefinitionRegistry getRegistry();
